4b70066602c3145dd75a447b845d30db01afcc3e
badcommit <zhangshe@usc.edu>
2021-02-07 01:08:57 +0800

    fix eval code

--- dataset/cifar10loader.py
+++ dataset/cifar10loader.py
@@ -4,6 +4,8 @@ import os.path as osp
 from torch.utils.data import DataLoader
 import torch
 import torchvision
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 defaultTransform = torchvision.transforms.Compose([
                                            torchvision.transforms.ToTensor()
@@ -16,15 +18,25 @@ class Cifar10Loader(object):
             root_dir (string): Directory with all the images.
         """
         dt = torchvision.datasets.CIFAR10(root_dir, download=True, transform=defaultTransform)
+        self.dt = dt
 
-        datalen = len(dt)
-        trainlen = int(datalen*(1-split_coef))
-        testlen = int(datalen-trainlen)
-        train, test = torch.utils.data.random_split(dt, (trainlen, testlen))
-        self.train = DataLoader(train, batch_size=batch_size, shuffle=True, drop_last=True)
-        self.test = DataLoader(test, batch_size=batch_size, shuffle=True, drop_last=True)
         self.num_classes = 10
-        self.train_sz = trainlen // batch_size * batch_size
-        self.test_sz = testlen // batch_size * batch_size
+        self.train_sz = 500*self.num_classes // batch_size * batch_size
+        self.test_sz = 1000*self.num_classes // batch_size * batch_size
+
+        targets = np.array(self.dt.targets)
+        target_indices = np.arange(len(targets))
+        train_idx, val_idx = train_test_split(target_indices, train_size=500*self.num_classes, stratify=targets)
+        query_idx, ret_idx = train_test_split(val_idx, train_size=1000*self.num_classes, stratify=targets[val_idx])
+
+        train = torch.utils.data.Subset(self.dt, train_idx)
+        query = torch.utils.data.Subset(self.dt, query_idx)
+        retrieve = torch.utils.data.Subset(self.dt, ret_idx)
+
+        self.train = DataLoader(train, batch_size=batch_size, shuffle=True, drop_last=True)
+        self.query = DataLoader(query, batch_size=batch_size, shuffle=True, drop_last=True)
+        self.retrieve = DataLoader(retrieve, batch_size=batch_size, shuffle=True, drop_last=True)
+
+
 
 
